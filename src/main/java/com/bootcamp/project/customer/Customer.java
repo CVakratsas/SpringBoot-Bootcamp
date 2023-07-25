e15889b59a7d7fa51d16ef87452681737174e76b
Constantinos Vakratsas <c.vakratsas@gmail.com>
2023-07-25 12:17:38 +0300

    Organized files to obey the N tier architecture

---
+++ src/main/java/com/bootcamp/project/customer/Customer.java
@@ -0,0 +1,27 @@
+package com.bootcamp.project.customer;
+
+public class Customer {
+    private final long id;
+    private final String name;
+
+    public Customer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/bootcamp/project/customer/CustomerRepository.java
@@ -0,0 +1,4 @@
+package com.bootcamp.project.customer;
+
+public class CustomerRepository {
+}
