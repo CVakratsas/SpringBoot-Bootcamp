d7173208c643635c62eeda678b79050cb49edc82
Constantinos Vakratsas <c.vakratsas@gmail.com>
2023-07-25 11:55:03 +0300

    Created a simple REST API request

--- src/main/java/com/bootcamp/project/ProjectApplication.java
+++ src/main/java/com/bootcamp/project/ProjectApplication.java
@@ -2,12 +2,37 @@ package com.bootcamp.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
+	@GetMapping
+	Customer getCustomer() {
+		return new Customer(1L, "James Bond");
+	}
+
+	class Customer {
+		private final long id;
+		private final String name;
+
+		public Customer(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+	}
 }
