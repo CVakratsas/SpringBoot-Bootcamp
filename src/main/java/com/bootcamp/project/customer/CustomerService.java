88b44402562b8d841693f8bd0a221a2c8f963317
CVakratsas <c.vakratsas@gmail.com>
2023-07-30 00:02:23 +0300

    Added dependency injection

--- src/main/java/com/bootcamp/project/customer/CustomerService.java
+++ src/main/java/com/bootcamp/project/customer/CustomerService.java
@@ -1,4 +1,11 @@
 package com.bootcamp.project.customer;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerService {
+
+    Customer getCustomer() {
+        return new Customer(1L, "James Bond");
+    }
 }
